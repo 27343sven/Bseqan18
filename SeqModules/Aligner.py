e2b4bae2c4c4be38e13bc6c0664eefde5ee72436
Sven Bijleveld <sven@debijleveldjes.nl>
2018-09-20 13:48:50 +0200

    answer class can now print the alignment.

--- SeqModules/Aligner.py
+++ SeqModules/Aligner.py
@@ -17,9 +17,8 @@ class Aligner:
         self.seq2 = "*" + self.seq2
         self.__fill_matrices()
         self.__calulate_scores()
-        print(self.scoreMatrix)
-        print(self.directionMatrix)
-        self.__traceback()
+        answer = self.__traceback()
+        return answer
 
     def __calulate_scores(self):
         for i in range(1, self.scoreMatrix.shape[0]):
@@ -35,27 +34,19 @@ class Aligner:
         x = self.scoreMatrix.shape[1]
         _seq1, _seq2, i, j = "", "", self.scoreMatrix.shape[0]-1, self.scoreMatrix.shape[1]-1
         while (i, j) != (0, 0):
-            print("===({}, {})===".format(i, j))
             directions = np.array(list(bin(self.directionMatrix[i, j])[2:].zfill(3)), dtype=np.int)
             values = np.take(self.scoreMatrix, [max(0, i * x + j - 1),
                                                 max(0, (i - 1) * x + j - 1),
                                                 max(0, (i - 1) * x + j)])
-            print(directions)
-            print(values)
-            values = np.ma.masked_array(values, np.logical_not(directions[directions]))
+            values = np.ma.masked_array(values, np.logical_not(directions))
             np.ma.set_fill_value(values, min(values) - 1)
             new_dir = np.ma.argmax(values)
-            print(new_dir)
             _seq1 += "_" if new_dir == 0 else self.seq1[i]
             _seq2 += "_" if new_dir == 2 else self.seq2[j]
             i = i - 0 if new_dir == 0 else i - 1
             j = j - 0 if new_dir == 2 else j - 1
-            print(_seq1[::-1])
-            print(_seq2[::-1])
-
-
-
-
+        answer = AlignementResult(self.seq1, self.seq2, _seq1, _seq2, self.scoreMatrix, self.directionMatrix)
+        return answer
 
     def __fill_matrices(self):
         self.scoreMatrix = np.zeros((len(self.seq1), len(self.seq2)), dtype=np.int)
@@ -64,7 +55,6 @@ class Aligner:
         self.scoreMatrix[:, 0] = np.arange(0, self.scoreMatrix.shape[0] * self.penalties["*"], self.penalties["*"])
         self.directionMatrix[0, 1:] = np.full(self.directionMatrix.shape[1] - 1, int('100', 2))
         self.directionMatrix[1:, 0] = np.full(self.directionMatrix.shape[0] - 1, int('001', 2))
-
 
     def __checkSequence(self):
         self.seq1 = self.seq1.upper()
@@ -75,6 +65,33 @@ class Aligner:
         if self.seq1.strip("ATCG") != "" or self.seq2.strip("ATCG") != "":
             raise SequenceException("One or both of the sequences contain illegal characters.")
 
+class AlignementResult:
+
+    def __init__(self, seq1, seq2, al_seq1, al_seq2, scoreMatrix, directionMatrix):
+        self.seq1 = seq1
+        self.seq2 = seq2
+        self.al_seq1 = al_seq1
+        self.al_seq2 = al_seq2
+        self.scoreMatrix = scoreMatrix
+        self.directionmatrix = directionMatrix
+
+    def show_scorematrix(self):
+        print(self.scoreMatrix)
+
+    def show_directionmatrix(self):
+        print(self.directionmatrix)
+
+    def show_alignment(self):
+        print(self.al_seq1)
+        for i in range(len(self.al_seq1)):
+            if self.al_seq1[i] == self.al_seq2[i]:
+                print("|", end="")
+            elif self.al_seq1[i] + self.al_seq2[i] in ["TC", "CT", "CG", "GC"]:
+                print("*", end="")
+            else:
+                print(" ", end="")
+        print()
+        print(self.al_seq2)
 
 
 class SequenceException(Exception):
